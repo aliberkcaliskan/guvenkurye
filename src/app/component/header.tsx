472174942a5dfa2cc7901fb5dd467a1351b7098c
Ali Berk Çalışkan <56911685+aliberkcaliskan@users.noreply.github.com>
2024-01-07 23:56:03 +0300

    image

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -8,6 +8,7 @@ const Header: React.FC = () => {
       <div className="h-24 z-20 relative container mx-auto flex items-center justify-between px-6">
         <div className=" cursor-pointer flex :items-center justify-center md:justify-normal md:items-start w-full">
           <Image 
+          className="w-[300px]"
             src="/logo.webp"
             alt="logo"
             width={300}
