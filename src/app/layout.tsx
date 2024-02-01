ad18d3ff920e67ff6f54cdc11abd1fbb58e5ad19
Ali Berk Çalışkan <56911685+aliberkcaliskan@users.noreply.github.com>
2024-02-02 00:53:13 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,38 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import {getAnalytics} from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import "./globals.css";
+const inter = Inter({subsets: ["latin"]});
 
-const inter = Inter({ subsets: ['latin'] })
+const firebaseConfig = {
+  apiKey: "AIzaSyCCkd7tVIxjbgiCNhgvummWnVJyywoHJEg",
+  authDomain: "guven-kurye.firebaseapp.com",
+  projectId: "guven-kurye",
+  storageBucket: "guven-kurye.appspot.com",
+  messagingSenderId: "472423439478",
+  appId: "1:472423439478:web:f09213c4356e8ac8a7d357",
+  measurementId: "G-H89WXZQ976",
+};
+// Initialize Firebase
 
-export const metadata: Metadata = {
-  title: 'Güven Kurye - Hızlı ve Güvenilir Teslimat',
-  description: 'Güven Kurye, şehir içi hızlı paket teslimatı sağlayan bir kurye hizmetidir. Güvenli ve zamanında teslimatlarımızla, 1 noktadan diğer noktaya paketlerinizi güvenle ulaştırıyoruz.',
-  keywords: 'güven kurye, hızlı teslimat, şehir içi kurye, paket teslimatı, güvenilir kurye hizmeti',
+const app = initializeApp(firebaseConfig);
+if (typeof window !== 'undefined') {
+  getAnalytics(app);
+
 }
+export const metadata: Metadata = {
+  title: "Güven Kurye - Hızlı ve Güvenilir Teslimat",
+  description:
+    "Güven Kurye, şehir içi hızlı paket teslimatı sağlayan bir kurye hizmetidir. Güvenli ve zamanında teslimatlarımızla, 1 noktadan diğer noktaya paketlerinizi güvenle ulaştırıyoruz.",
+  keywords:
+    "güven kurye, hızlı teslimat, şehir içi kurye, paket teslimatı, güvenilir kurye hizmeti",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="tr">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
