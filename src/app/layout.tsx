107a50eb41c40eb82e13c39558bc9f0b42463fe5
aliberkcaliskan <aliberkcaliskan40@gmail.com>
2024-06-03 04:19:06 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,11 +29,11 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-H89WXZQ976');
-            `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H89WXZQ976');
+          `,
           }}
         />
       </Head>
